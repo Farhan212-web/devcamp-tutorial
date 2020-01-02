9498c462d206e39e8b55e05748889497d5050f53
Farhan Emad <farhan480@hotmail.co.uk>
2020-01-02 00:34:49 +0000

    Ready for Data Flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :portfolios
+  get 'pages/home'
+  #When this route executes, it triggers home function in pages controller which can then 
+  #communicate with view
+  get 'pages/about'
+  get 'pages/contact'
+  resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
