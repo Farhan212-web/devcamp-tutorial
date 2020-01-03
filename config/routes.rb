b2bf84c909c101020040513e5cb3408d5ec78b30
Farhan Emad <farhan480@hotmail.co.uk>
2020-01-03 00:22:41 +0000

    Ready for Data Management

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'pages/home'
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'	
   #When this route executes, it triggers home function in pages controller which can then 
   #communicate with view
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+
+  resources :blogs do
+  	member do
+  		get :toggle_status
+  	end
+  end		
+
+  root to: 'pages#home'
 end
